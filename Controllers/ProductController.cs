e64c76069abec379cb6aca6fee89e382f56e3f76
Roman Dulin <63359306+Cyber-Roman@users.noreply.github.com>
2024-01-12 23:25:49 +0200

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>
+            {
+                new Product { ID = 1, Name = "Xiaomi", Price = 50.99, CreatedDate = DateTime.Now },
+                new Product { ID = 2, Name = "Apple", Price = 1500.80, CreatedDate = DateTime.Now.AddMinutes(-15) },
+                new Product { ID = 3, Name = "Asus", Price = 45.99, CreatedDate = DateTime.Now.AddDays(-2) }
+            };
+            return View(product);
+        }
+    }
+}
